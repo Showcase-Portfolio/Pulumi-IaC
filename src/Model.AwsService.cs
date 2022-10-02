a87d0eb8a2bff16fb189da8997b40a9bd24fe87d
Basam <96620145+Basam97@users.noreply.github.com>
2022-10-02 15:12:33 +1100

    modified to provision s3 and sns

---
+++ src/Model.AwsService.cs
@@ -0,0 +1,5 @@
+public class AwsServices
+{
+    public bool S3 { get; set; }
+    public bool Sns { get; set; }
+}
---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+﻿using Pulumi;
+using Pulumi.Aws.S3;
+using Pulumi.Aws.Sns;
+using Pulumi.Aws.DynamoDB;
+using System.Collections.Generic;
+using System.Text.Json;
+
+return await Deployment.RunAsync(() =>
+{
+    var config = new Pulumi.Config();
+    var services = config.GetObject<AwsServices>("awsServices")!;
+
+    var deployed = new Dictionary<string, object?>();
+
+    if (services.S3)
+    {
+        var bucket = new Bucket("sample-bucket");
+        deployed.Add("bucketName", bucket.Id);
+    }
+
+    if (services.Sns)
+    {
+        var sns = new Topic("sample-topic");
+        deployed.Add("snsName", sns.Id);
+    }
+
+    return deployed;
+});
